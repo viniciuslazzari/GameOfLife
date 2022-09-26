4d7ed56b161e3b7727557b6e09b4de6fead5cfc4
Vinícius Lazzari <vinilazzari028@gmail.com>
2022-09-26 20:39:56 -0300

    💄 Change windows icon

--- src/main.c
+++ src/main.c
@@ -1,11 +1,17 @@
 #include "game.h"
 
+const char* iconPath = "assets/icon.png";
+
 int main(void){
     Cell grid[MAX][MAX];
 
     initGame(grid);
 
+    Image windowsIcon = LoadImage(iconPath);
+
     InitWindow(SCREEN_SIZE, SCREEN_SIZE, "Life");
+
+    SetWindowIcon(windowsIcon);
 
     SetTargetFPS(5);
 
